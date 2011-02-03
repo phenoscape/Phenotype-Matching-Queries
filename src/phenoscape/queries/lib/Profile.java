349d9e47449bc7b3277c7ccaaee82f21184638dd
Peter E. Midford <peter.midford@gmail.com>
2011-02-03 09:40:15 -0500

    Internal rework (now entity major, not attribute major).  Useful because the list of used entities is more interesting and larger than the set of attributes used.

--- src/phenoscape/queries/lib/Profile.java
+++ src/phenoscape/queries/lib/Profile.java
@@ -7,50 +7,50 @@ import java.util.Set;
 
 public class Profile {
 	
-	private Map<Integer,Map<Integer,Set<Integer>>> table = new HashMap<Integer,Map<Integer,Set<Integer>>>();  //Attributes, entities, phenotypes
+	private Map<Integer,Map<Integer,Set<Integer>>> table = new HashMap<Integer,Map<Integer,Set<Integer>>>();  //entities, attributes phenotypes
 	
 	
-	public void addPhenotype(Integer attribute_node_id, Integer entity_node_id, Integer phenotype_node_id){
-		if (table.containsKey(attribute_node_id)){
-			Map<Integer,Set<Integer>> attribute_entry = table.get(attribute_node_id);
-			if (attribute_entry.containsKey(entity_node_id)){
-				Set<Integer> phenotypeSet = attribute_entry.get(entity_node_id);
+	public void addPhenotype(Integer entity_node_id, Integer attribute_node_id, Integer phenotype_node_id){
+		if (table.containsKey(entity_node_id)){
+			Map<Integer,Set<Integer>> entity_entry = table.get(entity_node_id);
+			if (entity_entry.containsKey(attribute_node_id)){
+				Set<Integer> phenotypeSet = entity_entry.get(attribute_node_id);
 				phenotypeSet.add(phenotype_node_id);
 			}
 			else {
 				Set<Integer> phenotypeSet = new HashSet<Integer>();
 				phenotypeSet.add(phenotype_node_id);
-				attribute_entry.put(entity_node_id,phenotypeSet);
+				entity_entry.put(attribute_node_id,phenotypeSet);
 			}
 		}
 		else {
-			Map<Integer,Set<Integer>> attribute_entry = new HashMap<Integer,Set<Integer>>();
+			Map<Integer,Set<Integer>> entity_entry = new HashMap<Integer,Set<Integer>>();
 			Set<Integer> phenotypeSet = new HashSet<Integer>();
 			phenotypeSet.add(phenotype_node_id);
-			attribute_entry.put(entity_node_id,phenotypeSet);
-			table.put(attribute_node_id, attribute_entry);
+			entity_entry.put(attribute_node_id,phenotypeSet);
+			table.put(entity_node_id, entity_entry);
 		}
 	}
 	
-	public void addAlltoPhenotypeSet(Integer attribute_node_id, Integer entity_node_id,Set<Integer> toAdd){
-		if (table.containsKey(attribute_node_id)){
-			Map<Integer,Set<Integer>> attribute_entry = table.get(attribute_node_id);
-			if (attribute_entry.containsKey(entity_node_id)){
-				Set<Integer> phenotypeSet = attribute_entry.get(entity_node_id);
+	public void addAlltoPhenotypeSet(Integer entity_node_id, Integer attribute_node_id,Set<Integer> toAdd){
+		if (table.containsKey(entity_node_id)){
+			Map<Integer,Set<Integer>> entity_entry = table.get(entity_node_id);
+			if (entity_entry.containsKey(attribute_node_id)){
+				Set<Integer> phenotypeSet = entity_entry.get(attribute_node_id);
 				phenotypeSet.addAll(toAdd);
 			}
 			else {
 				Set<Integer> phenotypeSet = new HashSet<Integer>();
 				phenotypeSet.addAll(toAdd);
-				attribute_entry.put(entity_node_id,phenotypeSet);
+				entity_entry.put(attribute_node_id,phenotypeSet);
 			}
 		}
 		else {
-			Map<Integer,Set<Integer>> attribute_entry = new HashMap<Integer,Set<Integer>>();
+			Map<Integer,Set<Integer>> entity_entry = new HashMap<Integer,Set<Integer>>();
 			Set<Integer> phenotypeSet = new HashSet<Integer>();
 			phenotypeSet.addAll(toAdd);
-			attribute_entry.put(entity_node_id,phenotypeSet);
-			table.put(attribute_node_id, attribute_entry);
+			entity_entry.put(attribute_node_id,phenotypeSet);
+			table.put(entity_node_id, entity_entry);
 		}
 	}
 	
@@ -64,13 +64,13 @@ public class Profile {
 		int count = 0;
 		int fillCount = 0;
 		int entries = 0;
-		for(Integer att : getUsedAttributes()){
-			for(Integer ent : getUsedEntities()){
-				if (hasPhenotypeSet(att,ent)){
+		for(Integer ent : getUsedEntities()){
+			for(Integer att : getUsedAttributes()){
+				if (hasPhenotypeSet(ent,att)){
 					count++;
-					if (getPhenotypeSet(att,ent).size()>0){
+					if (getPhenotypeSet(ent,att).size()>0){
 						fillCount++;
-						entries += getPhenotypeSet(att,ent).size();
+						entries += getPhenotypeSet(ent,att).size();
 					}
 				}
 			}
@@ -79,34 +79,35 @@ public class Profile {
 		return b.toString();
 	}
 	
-	public boolean hasPhenotypeSet(Integer attribute, Integer entity){
-		if (table.containsKey(attribute))
-			return (table.get(attribute).containsKey(entity));
+	public boolean hasPhenotypeSet(Integer entity, Integer attribute){
+		if (table.containsKey(entity))
+			return (table.get(entity).containsKey(attribute));
 		else
 			return false;
 	}
-	public Set<Integer> getPhenotypeSet (Integer attribute, Integer entity){
-		return table.get(attribute).get(entity);
-	}
 	
-	public Set<Integer> getUsedAttributes(){
-		return table.keySet();
+	public Set<Integer> getPhenotypeSet (Integer entity, Integer attribute){
+		return table.get(entity).get(attribute);
 	}
 	
 	public Set<Integer> getUsedEntities(){
+		return table.keySet();
+	}
+	
+	public Set<Integer> getUsedAttributes(){
 		Set<Integer>result = new HashSet<Integer>();
-		for (Map<Integer,Set<Integer>> attribute_value : table.values()){
-			result.addAll(attribute_value.keySet());
+		for (Map<Integer,Set<Integer>> entity_value : table.values()){
+			result.addAll(entity_value.keySet());
 		}
 		return result;
 	}
 	
 	public Set<Integer> getAllPhenotypes(){
 		Set<Integer> result = new HashSet<Integer>();
-		for (Integer curAtt : getUsedAttributes()){ 
-			Map<Integer,Set<Integer>> attValue = table.get(curAtt);
-			for (Set<Integer> entValue : attValue.values()){
-				result.addAll(entValue);
+		for (Integer curEnt : getUsedEntities()){ 
+			Map<Integer,Set<Integer>> entValue = table.get(curEnt);
+			for (Set<Integer> attValue : entValue.values()){
+				result.addAll(attValue);
 			}
 		}
 		return result;
